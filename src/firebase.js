0d74d035289b13a13cbd43cb6d080988202e31a6
Mats6102hamberg <mhg10mhg@gmail.com>
2025-10-04 21:30:43 +0200

    Update Firebase config to use new env variable name

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase-konfiguration från miljövariabler
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || '',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY_NEW || import.meta.env.VITE_FIREBASE_API_KEY || '',
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || '',
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || '',
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || '',
